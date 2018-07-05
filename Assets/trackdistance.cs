c464ece673ea678f21a4cccc4d22bde512bbe4f9
dave <david.marshall@swc.ac.uk>
2018-07-05 14:58:21 +0100

    Commit

---
+++ Assets/trackdistance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackdistance : MonoBehaviour {
+
+    public float distanceTravelled = 0;
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start () {
+        lastPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
+    }
+}
